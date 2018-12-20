5608dcb742609ad8780046c48c834aa86175f090
Pranav Jangir <jangir.pranav@gmail.com>
2018-12-20 15:16:20 +0530

    Update TIMUS 1619.cpp

--- Timus/TIMUS 1619.cpp
+++ Timus/TIMUS 1619.cpp
@@ -1,33 +1,18 @@
+/*
+        This problem is a direct application of Bertrand's ballot theorem, 
+        for (k,m), ans is (k-m+1)/(k+1)
+        proof : https://en.wikipedia.org/wiki/Bertrand's_ballot_theorem#Variant:_ties_allowed
+*/
+
 #include <bits/stdc++.h>
 typedef long long ll;
 typedef long double ld;
 using namespace std;
-ld fac[30005];
-void init()
-{
-        fac[0] = 1;
-        for(ll i=1;i<=20002;i++) fac[i] = fac[i-1] * i;
-}
-ld C(ll x, ll y)
-{
-        if(x < y) return 1.0;
-        ld ret = fac[x];
-        ret = ret / fac[y];
-        ret = ret / fac[x-y];
-        return ret;
-}
-ld cats(ll x)
-{
-        ld ret = C(x+x,x);
-        ret /= (ld)(1.0 + 1.0*x);
-        return ret;
-}
 int main()
 {
         cout << setprecision(8) << fixed;
         ll t;
         cin >> t;
-        init();
         while(t--) {
                 ll a,b;
                 cin >> a >> b;
