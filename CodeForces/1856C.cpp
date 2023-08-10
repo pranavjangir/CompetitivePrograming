fc5fbbb5ec3a2f67a327c656f8b7b2589aeef3d0
Pranav <pranav@MacBook-Pro.local>
2023-08-10 22:15:35 +0530

    Correct solution for problem C added.

--- CodeForces/1856C.cpp
+++ CodeForces/1856C.cpp
@@ -31,32 +31,50 @@ ll a[2002];
 ll dp[2002];
 ll steps[2002];
 
+bool check(ll x) {
+    for (ll i = n; i >= 1; --i) {
+        ll stps = 0;
+        bool ok = true;
+        ll cur = x;
+        for (ll j = i; j <= n; ++j) {
+            if (j == n && a[j] < cur) {
+                ok = false;
+                break;
+            }
+            if (a[j] >= cur) {
+                break;
+            } else {
+                stps += cur - a[j];
+                cur--;
+            }
+        }
+        if (!ok) {
+            continue;
+        }
+        if (stps <= k) {
+            return true;
+        }
+    }
+    return false;
+}
+
 void solve() {
     cin >> n >> k;
     for (ll i = 1; i <= n; ++i) cin >> a[i];
-    dp[n] = a[n];
-    steps[n] = 0;
-    for (ll i = n - 1 ; i >= 1; i--) {
-        if (dp[i + 1] + 1 > a[i]) {
-            dp[i] = dp[i + 1] + 1;
-            steps[i] = steps[i + 1] + (dp[i + 1] - a[i] + 1);
-        } else {
-            dp[i] = a[i];
-            steps[i] = 0;
-        }
+    ll lo = 0;
+    ll hi = 2e8+1;
+    while (hi > lo) {
+        ll mid = (hi + lo + 1) / 2;
+        if (check(mid)) {
+            lo = mid;
+        } else hi = mid - 1;
     }
-    ll ans = -1;
-    for (ll i = n ; i >= 1; --i) {
-        cout << i << " -- " << steps[i] << " " << dp[i] << "\n";
-        if (steps[i] <= k) {
-            ans = max(ans, dp[i]);
-            // continue;
-        }
-    }
-    cout << ans << "\n";
+    cout << lo << "\n";
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
     cin >> t;
     while (t--) solve();
     return 0;
